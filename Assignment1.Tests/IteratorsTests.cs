5e6a24a6fe238e211978bc982020611b57abad43
Thomas <thwr@itu.dk>
2021-09-10 13:02:17 +0200

    add flattten test

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -2,7 +2,20 @@ using Xunit;
 
 namespace Assignment1.Tests
 {
-    public class IteratorsTests
+    public class Given_stream_of_T_return_Stream_T
     {
+            //arrange
+            // IEnumerable<t> input = new IEnumerable<t>; 
+            var input = new [][] {
+                [0] {1,2,3,4,5}
+                [1] {5,4,3,2,1}
+            };
+
+            //act
+            var output = Flatten(input.getEnumerator());
+
+            //assert
+            Assert.Equals(input.getEnumerator(), output);
+
     }
 }
