56a8768afaf93e00f01ce581009c44366bc6b44d
CasparMarschall <cafm@itu.dk>
2021-09-10 13:43:01 +0200

    Added test for flatten

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,8 +1,20 @@
 using Xunit;
+using System.Collections.Generic;
 
 namespace Assignment1.Tests
 {
     public class IteratorsTests
     {
+        [Fact]
+        public void flatten_return_yield_returns_1_2_3_4_5_3times()
+        {
+            var input = new List<List<int>>(){ new List<int>(){ 1, 2, 3, 4, 5 }, new List<int>(){ 1, 2, 3, 4, 5 }, new List<int>(){ 1, 2, 3, 4, 5 } };
+
+
+            var output = Assignment1.Iterators.Flatten<int>(input);
+            var expected = new List<int>() { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5 };
+
+            Assert.Equal(expected, output);
+        }
     }
 }
