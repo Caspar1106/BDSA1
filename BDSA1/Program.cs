f7c20151c0cdba4646cb7e99a84e5fe95d9e484d
CasparMarschall <cafm@itu.dk>
2021-09-10 12:13:08 +0200

    Added project and test folder

---
+++ BDSA1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BDSA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
