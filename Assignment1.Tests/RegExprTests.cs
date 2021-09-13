0ad3e4c19d6fc88033b891fb6a0122110d13b2d6
Michael Tran <michaelhieu@hotmail.com>
2021-09-13 15:53:40 +0200

    minor detail

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -7,12 +7,12 @@ namespace Assignment1.Tests
     public class RegExprTests
     {
         [Fact]
-        public void regTest() {
+        public void splitLine_returns_stream_of_words() {
 
-            var input = new List<string>(){"Hello World", "Hello", "Hello great world"};
+            var input = new List<string>(){"Hello World", "Hello", "Hello great world123"};
             var output = Assignment1.RegExpr.SplitLine(input);
 
-            Assert.Equal(new List<string>() {"Hello", "World", "Hello", "Hello", "great", "world" }, output);
+            Assert.Equal(new List<string>() {"Hello", "World", "Hello", "Hello", "great", "world123" }, output);
         }
 
     }
